337f81e6a1a5997ec4bbb9c3a1fb4fd6999f4502
tiger9tu <76516415+tiger9tu@users.noreply.github.com>
2024-06-29 20:55:22 +0800

    succeess

--- hhl.py
+++ hhl.py
@@ -1,12 +1,10 @@
 import numpy as np
-import scipy as sp
 
 # Import Qiskit
-from qiskit import QuantumCircuit, transpile, QuantumRegister, ClassicalRegister
-from qiskit_aer import AerSimulator
-from qiskit.visualization import plot_histogram
-from qiskit.quantum_info import Operator, Statevector
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.quantum_info import  SparsePauliOp
 
+from qiskit.circuit.library import PauliEvolutionGate
 from qiskit.circuit.library.phase_estimation import PhaseEstimation
 from qiskit.circuit.library.arithmetic.exact_reciprocal import ExactReciprocal
 
@@ -31,7 +29,10 @@ class HHL(QuantumCircuit):
         self._vector = vector
         self._tolerance = tolerance
 
+        self.scaling = 1 
+        self.neg_eigenvals = True 
         # build circuit
+        self._check_configuration()
         self._build()
 
     def eigs_bounds(self):
@@ -46,6 +47,10 @@ class HHL(QuantumCircuit):
         matrix_array = self._matrix
         kappa = np.linalg.cond(matrix_array)
         return kappa, kappa
+    
+    def _get_ejHt(self, evolution_time):
+        pauli_op = SparsePauliOp.from_operator(self._matrix)
+        return PauliEvolutionGate(pauli_op, -evolution_time)
 
     def _get_delta(self, n_l: int, lambda_min: float, lambda_max: float) -> float:
         """Calculates the scaling factor to represent exactly lambda_min on nl binary digits.
@@ -58,17 +63,16 @@ class HHL(QuantumCircuit):
         Returns:
             The value of the scaling factor.
         """
-        # formatstr = "#0" + str(n_l + 2) + "b"
-        # lambda_min_tilde = np.abs(lambda_min * (2**n_l - 1) / lambda_max)
-        # # floating point precision can cause problems
-        # if np.abs(lambda_min_tilde - 1) < 1e-7:
-        #     lambda_min_tilde = 1
-        # binstr = format(int(lambda_min_tilde), formatstr)[2::]
-        # lamb_min_rep = 0
-        # for i, char in enumerate(binstr):
-        #     lamb_min_rep += int(char) / (2 ** (i + 1))
-        # return lamb_min_rep
-        return lambda_min
+        formatstr = "#0" + str(n_l + 2) + "b"
+        lambda_min_tilde = np.abs(lambda_min * (2**n_l - 1) / lambda_max)
+        # floating point precision can cause problems
+        if np.abs(lambda_min_tilde - 1) < 1e-7:
+            lambda_min_tilde = 1
+        binstr = format(int(lambda_min_tilde), formatstr)[2::]
+        lamb_min_rep = 0
+        for i, char in enumerate(binstr):
+            lamb_min_rep += int(char) / (2 ** (i + 1))
+        return lamb_min_rep
 
     def _check_configuration(self, raise_on_failure: bool = True) -> bool:
         """Check if the current configuration is valid."""
@@ -92,13 +96,12 @@ class HHL(QuantumCircuit):
     def _build(self):
         num_vec_qubits = np.log2(len(self._vector))
         kappa = self.condition_bounds()[0]
-        num_clock_qubits = int(max(num_vec_qubits + 1, int(np.ceil(np.log2(kappa + 1)))))
+        num_clock_qubits = int(max(num_vec_qubits + 1, int(np.ceil(np.log2(kappa + 1))))) + self.neg_eigenvals
         lambda_min, lambda_max = self.eigs_bounds()
-        delta = self._get_delta(num_clock_qubits, lambda_min, lambda_max)
-        evolution_time = 2 * np.pi * delta / lambda_min
+        delta = self._get_delta(num_clock_qubits - self.neg_eigenvals, lambda_min, lambda_max)
+        evolution_time = 2 * np.pi * delta / lambda_min / (2**self.neg_eigenvals)
 
-        # Update the scaling of the solution
-        self._scaling = lambda_min
+        self.scaling = lambda_min
 
         qr_vec = QuantumRegister(num_vec_qubits)
         qr_clock = QuantumRegister(num_clock_qubits)
@@ -109,27 +112,26 @@ class HHL(QuantumCircuit):
         qc_vec = QuantumCircuit(qr_vec)
         vec_norm = np.linalg.norm(self._vector)
         qc_vec.prepare_state(self._vector / vec_norm )
-        self._scaling *= vec_norm 
+        self.scaling /= vec_norm 
         qc.append(qc_vec, qr_vec[:])
 
         qc_mat = QuantumCircuit(qr_vec)
-        e_mat = sp.linalg.expm(1j * self._matrix * evolution_time)
-        qc_mat.append(Operator(e_mat).to_instruction(),[0])
+
+        e_mat_op = self._get_ejHt(evolution_time)
+        qc_mat.append(e_mat_op,range(qc_mat.num_qubits))
 
         qc_pe_e_mat = PhaseEstimation(num_clock_qubits,qc_mat)
         qc.append(qc_pe_e_mat, qr_clock[:] + qr_vec[:])
 
-        qc_cr = ExactReciprocal(num_clock_qubits, delta)
+        qc_cr = ExactReciprocal(num_clock_qubits, delta, neg_vals=self.neg_eigenvals)
         qc.append(qc_cr.to_instruction(),  qr_clock[::-1] + [qr_flag[0]])
-
+        
         qc.append(qc_pe_e_mat.inverse(), qr_clock[:] + qr_vec[:])
 
         self.add_register(*qc.qregs)
         self.compose(qc,  inplace=True)
 
 
-A = np.array([[1,0],[0,1]]) 
-b_ = np.array([7,1]) 
-qc = HHL(A, b_)
 
-print(qc)
+
+
