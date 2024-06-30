5c63ca180c2132e515e18f0eb2ef808aa25e447c
tiger9tu <76516415+tiger9tu@users.noreply.github.com>
2024-06-30 17:36:52 +0800

    improve

--- hhl.py
+++ hhl.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Import Qiskit
 from qiskit import QuantumCircuit, QuantumRegister
-from qiskit.quantum_info import  SparsePauliOp
+from qiskit.quantum_info import SparsePauliOp
 
 from qiskit.circuit.library import PauliEvolutionGate
 from qiskit.circuit.library.phase_estimation import PhaseEstimation
@@ -25,36 +25,40 @@ class HHL(QuantumCircuit):
     ) -> None:
         super().__init__(name=name)
 
-        self._matrix = matrix
-        self._vector = vector
-        self._tolerance = tolerance
+        self.matrix = matrix
+        self.vector = vector
+        self.tolerance = tolerance
 
-        self.scaling = 1 
-        self.neg_eigenvals = True 
+        self.scaling = 1
+        self.neg_eigenvals = True
+
+        self.qc_pe_ejHt = None
+
         # build circuit
         self._check_configuration()
+        self._set_qc_param()
         self._build()
 
-    def eigs_bounds(self):
-        """Return lower and upper bounds on the eigenvalues of the matrix."""
-        matrix_array = self._matrix
-        lambda_max = max(np.abs(np.linalg.eigvals(matrix_array)))
-        lambda_min = min(np.abs(np.linalg.eigvals(matrix_array)))
-        return lambda_min, lambda_max
+    def _check_configuration(self, raise_on_failure: bool = True) -> bool:
+        """Check if the current configuration is valid."""
+        valid = True
+        if self.matrix.shape[0] != self.matrix.shape[1]:
+            if raise_on_failure:
+                raise AttributeError("Input matrix must be square!")
+            return False
+        if np.log2(self.matrix.shape[0]) % 1 != 0:
+            if raise_on_failure:
+                raise AttributeError("Input matrix dimension must be 2^n!")
+            return False
+        if not np.allclose(self.matrix, self.matrix.conj().T):
+            if raise_on_failure:
+                raise AttributeError("Input matrix must be hermitian!")
+            return False
 
-    def condition_bounds(self):
-        """Return lower and upper bounds on the condition number of the matrix."""
-        matrix_array = self._matrix
-        kappa = np.linalg.cond(matrix_array)
-        return kappa, kappa
-    
-    def _get_ejHt(self, evolution_time):
-        pauli_op = SparsePauliOp.from_operator(self._matrix)
-        return PauliEvolutionGate(pauli_op, -evolution_time)
+        return valid
 
     def _get_delta(self, n_l: int, lambda_min: float, lambda_max: float) -> float:
         """Calculates the scaling factor to represent exactly lambda_min on nl binary digits.
-
         Args:
             n_l: The number of qubits to represent the eigenvalues.
             lambda_min: the smallest eigenvalue.
@@ -74,64 +78,63 @@ class HHL(QuantumCircuit):
             lamb_min_rep += int(char) / (2 ** (i + 1))
         return lamb_min_rep
 
-    def _check_configuration(self, raise_on_failure: bool = True) -> bool:
-        """Check if the current configuration is valid."""
-        valid = True
+    def _set_qc_param(self):
+        self.num_vec_qubits = np.log2(len(self.vector))
+        self.kappa = np.linalg.cond(self.matrix)
+        self.num_clock_qubits = (
+            int(max(self.num_vec_qubits + 1, int(np.ceil(np.log2(self.kappa + 1)))))
+            + self.neg_eigenvals
+        )
 
-        if self.matrix.shape[0] != self.matrix.shape[1]:
-            if raise_on_failure:
-                raise AttributeError("Input matrix must be square!")
-            return False
-        if np.log2(self.matrix.shape[0]) % 1 != 0:
-            if raise_on_failure:
-                raise AttributeError("Input matrix dimension must be 2^n!")
-            return False
-        if not np.allclose(self.matrix, self.matrix.conj().T):
-            if raise_on_failure:
-                raise AttributeError("Input matrix must be hermitian!")
-            return False
-
-        return valid
-
-    def _build(self):
-        num_vec_qubits = np.log2(len(self._vector))
-        kappa = self.condition_bounds()[0]
-        num_clock_qubits = int(max(num_vec_qubits + 1, int(np.ceil(np.log2(kappa + 1))))) + self.neg_eigenvals
-        lambda_min, lambda_max = self.eigs_bounds()
-        delta = self._get_delta(num_clock_qubits - self.neg_eigenvals, lambda_min, lambda_max)
-        evolution_time = 2 * np.pi * delta / lambda_min / (2**self.neg_eigenvals)
-
+        lambda_max = max(np.abs(np.linalg.eigvals(self.matrix)))
+        lambda_min = min(np.abs(np.linalg.eigvals(self.matrix)))
+        self.delta = self._get_delta(
+            self.num_clock_qubits - self.neg_eigenvals, lambda_min, lambda_max
+        )
+        self.evolution_time = (
+            2 * np.pi * self.delta / lambda_min / (2**self.neg_eigenvals)
+        )
         self.scaling = lambda_min
 
-        qr_vec = QuantumRegister(num_vec_qubits)
-        qr_clock = QuantumRegister(num_clock_qubits)
+    def _get_state_preparation(self):
+        qc_state_prepare = QuantumCircuit(self.num_vec_qubits)
+        vec_norm = np.linalg.norm(self.vector)
+        qc_state_prepare.prepare_state(self.vector / vec_norm)
+        self.scaling /= vec_norm
+        return qc_state_prepare
+
+    def _get_ejHt(self):
+        op_H = SparsePauliOp.from_operator(self.matrix)
+        op_ejHt = PauliEvolutionGate(op_H, -self.evolution_time)
+        qc_ejHt = QuantumCircuit(self.num_vec_qubits)
+        qc_ejHt.append(op_ejHt, range(int(self.num_vec_qubits)))
+        return qc_ejHt
+
+    def _get_QPE(self):
+        if self.qc_pe_ejHt is None:
+            qc_ejHt = self._get_ejHt()
+            self.qc_pe_ejHt = PhaseEstimation(self.num_clock_qubits, qc_ejHt)
+        return self.qc_pe_ejHt
+
+    def _get_controld_rotation(self):
+        qc_cr = ExactReciprocal(
+            self.num_clock_qubits, self.delta, neg_vals=self.neg_eigenvals
+        )
+        return qc_cr
+
+    def _build(self):
+        qr_state_prepare = QuantumRegister(self.num_vec_qubits)
+        qr_clock = QuantumRegister(self.num_clock_qubits)
         qr_flag = QuantumRegister(1)  # suppose there is only one flag qubit
+        qc = QuantumCircuit(qr_state_prepare, qr_clock, qr_flag)
 
-        qc = QuantumCircuit(qr_vec , qr_clock , qr_flag)
-
-        qc_vec = QuantumCircuit(qr_vec)
-        vec_norm = np.linalg.norm(self._vector)
-        qc_vec.prepare_state(self._vector / vec_norm )
-        self.scaling /= vec_norm 
-        qc.append(qc_vec, qr_vec[:])
-
-        qc_mat = QuantumCircuit(qr_vec)
-
-        e_mat_op = self._get_ejHt(evolution_time)
-        qc_mat.append(e_mat_op,range(qc_mat.num_qubits))
-
-        qc_pe_e_mat = PhaseEstimation(num_clock_qubits,qc_mat)
-        qc.append(qc_pe_e_mat, qr_clock[:] + qr_vec[:])
-
-        qc_cr = ExactReciprocal(num_clock_qubits, delta, neg_vals=self.neg_eigenvals)
-        qc.append(qc_cr.to_instruction(),  qr_clock[::-1] + [qr_flag[0]])
-        
-        qc.append(qc_pe_e_mat.inverse(), qr_clock[:] + qr_vec[:])
+        qc.append(self._get_state_preparation(), qr_state_prepare[:])
+        qc.append(self._get_QPE(), qr_clock[:] + qr_state_prepare[:])
+        qc.append(
+            self._get_controld_rotation().to_instruction(),
+            qr_clock[::-1] + [qr_flag[0]],
+        )
+        qc.append(self._get_QPE().inverse(), qr_clock[:] + qr_state_prepare[:])
 
         self.add_register(*qc.qregs)
-        self.compose(qc,  inplace=True)
-
-
-
-
-
+        self.compose(qc, inplace=True)
